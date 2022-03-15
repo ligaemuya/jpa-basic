a17663e2a5660b5366e1fcf539fe329f6e629b74
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-03-16 02:28:42 +0900

    CRUD와 페이징 방법

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,59 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션을 받아오지않으면 실제 DB에 저장되지않음
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            /* 저장
+            Member member = new Member();
+            member.setId(2l);
+            member.setName("HelloB");
+            //em.persist(member);
+
+             */
+            /* 조회
+            Member findMember = em.find(Member.class, 1l);
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName());
+             */
+            /* 삭제
+            Member findMember = em.find(Member.class, 1l);
+            em.remove(findMember);
+             */
+            /* 수정
+            Member findMember = em.find(Member.class, 1l);
+            findMember.setName("HelloJPA");
+             */
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            //code
+            em.close();
+        }
+        emf.close();
+    }
+}
