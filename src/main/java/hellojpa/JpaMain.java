73f9a6533a6cdd5a36c0bfb8f3b96e9ae432dcc4
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-03-18 22:03:40 +0900

    jpa

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,36 +17,46 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            /* 저장
+            /*
+            저장
             Member member = new Member();
             member.setId(2l);
             member.setName("HelloB");
             //em.persist(member);
 
-             */
-            /* 조회
+            조회
             Member findMember = em.find(Member.class, 1l);
             System.out.println("findMember = " + findMember.getId());
             System.out.println("findMember = " + findMember.getName());
-             */
-            /* 삭제
+
+            삭제
             Member findMember = em.find(Member.class, 1l);
             em.remove(findMember);
-             */
-            /* 수정
+
+            수정
             Member findMember = em.find(Member.class, 1l);
             findMember.setName("HelloJPA");
-             */
 
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+                                    .setFirstResult(5)
+                                    .setMaxResults(8)
+                                    .getResultList();
 
             for (Member member : result) {
                 System.out.println("member = " + member.getName());
             }
+            */
 
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            //영속
+            System.out.println("===BEFORE===");
+            em.persist(member);
+            em.detach(member);
+            System.out.println("===AFTER===");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
